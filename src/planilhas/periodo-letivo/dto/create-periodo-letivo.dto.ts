7e9d6973d477a0d87075fb24a6df7066246eae1b
Franck Patrick <franckpatrick2000@gmail.com>
2025-05-09 20:42:13 -0300

    DTO periodo-letivo

--- src/planilhas/periodo-letivo/dto/create-periodo-letivo.dto.ts
+++ src/planilhas/periodo-letivo/dto/create-periodo-letivo.dto.ts
@@ -1,1 +1,33 @@
-export class CreatePeriodoLetivoDto {}
+import {
+    IsString,
+    IsNotEmpty,
+    MaxLength,
+    Matches,
+
+  } from 'class-validator';
+  
+  export class CreatePeriodoLetivoDto {
+    @IsString({ message: 'A identificação deve ser uma string.' })
+    @IsNotEmpty({ message: 'O campo "Identificação" não pode estar vazio.' })
+    @MaxLength(20, { message: 'A "Identificação" deve ter no máximo 20 caracteres.' }) 
+    periodoLetivo: string;
+  
+    
+  
+    @IsString({ message: 'A data inicial deve ser uma string.' })
+    @IsNotEmpty({ message: 'O campo "Data Inicial" não pode estar vazio.' })
+    @Matches(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/, {
+      message: 'A "Data Inicial" deve estar no formato D/M/AAAA ou DD/MM/AAAA (ex: 1/1/2025 ou 01/01/2025).',
+    })
+    @MaxLength(10, { message: 'A "Data Inicial" deve ter no máximo 10 caracteres.'})
+    dataInicial: string; 
+  
+    @IsString({ message: 'A data final deve ser uma string.' })
+    @IsNotEmpty({ message: 'O campo "Data Final" não pode estar vazio.' })
+    @Matches(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/, {
+      message: 'A "Data Final" deve estar no formato D/M/AAAA ou DD/MM/AAAA (ex: 1/7/2025 ou 01/07/2025).',
+    })
+    @MaxLength(10, { message: 'A "Data Final" deve ter no máximo 10 caracteres.'})
+    dataFinal: string; 
+  
+  }
